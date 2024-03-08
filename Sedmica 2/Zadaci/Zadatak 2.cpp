2ce3c1114693ccf78c9d4a9158348625bf0a4bf3
tamiroladejo <koltam09@gmail.com>
2024-03-08 11:18:36 +0100

    Sedmica 2 Zadatak 2 uradjen.

--- Sedmica 2/Zadaci/Zadatak 2.cpp
+++ Sedmica 2/Zadaci/Zadatak 2.cpp
@@ -2,12 +2,14 @@
 
 std::string binarni(int a) {
     std::string binarni_broj = "";
+
     while (a != 0) {
         int c = a % 2;
         a /= 2;
         if (c == 0) binarni_broj = "0" + binarni_broj;
         else binarni_broj = "1" + binarni_broj;
     }
+
     return binarni_broj;
 }
 
