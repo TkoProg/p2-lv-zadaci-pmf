df66e7874385efba28944ff3bd0fb9367df951fb
tamiroladejo <koltam09@gmail.com>
2024-03-29 12:58:48 +0100

    Sedmica 5 Zadatak 2 uradjen ponovo na LV.

--- Sedmica 5/Zadaci/Zadatak 2.cpp
+++ Sedmica 5/Zadaci/Zadatak 2.cpp
@@ -4,13 +4,13 @@ template<typename Tip>
 void SmjestiMinIMax(Tip *pocetak, Tip *iza_kraja, Tip *&min, Tip *&max) {
     for(int *pok = pocetak; pok < iza_kraja; pok++) {
         if(*pok < *min) min = pok;
-        if(*pok > *max) max = pok;
+        else if(*pok >= *max) max = pok;
     }
 }
 
 int main() {
     int niz[]{6,2,4,8,6,1,3};
-    int *Min = &niz[0];
+    int *Min = &niz[0]; // Moglo je i int *Min{nullptr};
     int *Max = &niz[0];
 
     // Zanimljivo je ovo proslijedjivanje pointera u funkciju po referenci. Nisam znao da to moze!
