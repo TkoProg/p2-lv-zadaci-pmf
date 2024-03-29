fc290c7cf7424b1f3205e835fdfbad18302055f7
tamiroladejo <koltam09@gmail.com>
2024-03-29 12:34:18 +0100

    Sedmica 5 Zadatak 1 uradjen ponovo na LV.

--- Sedmica 5/Zadaci/Zadatak 1.cpp
+++ Sedmica 5/Zadaci/Zadatak 1.cpp
@@ -7,6 +7,20 @@ void Obrni(Tip *pocetak, Tip *iza_kraja){
     std::reverse(pocetak, iza_kraja);
 }
 
+template<typename Tip>
+void ObrniTeze(Tip *pocetak, Tip *iza_kraja){
+    Tip *lijevi = pocetak;
+    Tip* desni = iza_kraja - 1;
+    while (lijevi < desni) {
+        Tip temp = *lijevi;
+        *lijevi = *desni;
+        *desni = temp;
+        // Moze i swap funkcija umjesto ovoga
+        lijevi++;
+        desni--;
+    }
+}
+
 int main() {
     std::vector<int> v{1,2,3,4,5,6,7,8,9};
     int *pok1 = &v[2];
@@ -14,4 +28,5 @@ int main() {
 
     Obrni(pok1, pok2);
     for(auto i : v) std::cout << i << " ";
+    return 0;
 }
